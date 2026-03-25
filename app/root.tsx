c3d57de8e015f8d71387110635c61c80899422ce
ENG\Ahmed Eldesoky <ahmedeldesoky284@gmail.com>
2026-03-25 23:15:29 +0200

    Fix: Correct Polaris CSS import path

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { AppProvider } from "@shopify/polaris";
-import "@shopify/polaris/build/styles.css";
+import "@shopify/polaris/build/esm/styles.css";
 
 export default function App() {
   return (
