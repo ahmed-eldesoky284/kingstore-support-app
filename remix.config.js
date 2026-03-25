1e0d537749d92e76b9169e82596a8d188548cf9a
ENG\Ahmed Eldesoky <ahmedeldesoky284@gmail.com>
2026-03-25 23:02:43 +0200

    Final Vercel routing fix

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  serverModuleFormat: "cjs",
-  serverBuildPath: "build/index.js",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  serverBuildPath: "api/index.js",
   publicPath: "/build/",
   assetsBuildDirectory: "public/build",
 };
