e302e5e989eebe25ab523b51286537a0d45d34e2
ENG\Ahmed Eldesoky <ahmedeldesoky284@gmail.com>
2026-03-25 23:13:28 +0200

    Fix: Remove legacy server reference in remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,4 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  serverBuildPath: "api/index.js",
-  publicPath: "/build/",
-  assetsBuildDirectory: "public/build",
 };
